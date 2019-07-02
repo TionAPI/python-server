7a5a268481a634cbf572ab042d151139095ebc01
IATkachenko <IATkachenko@gmail.com>
2019-07-02 11:49:11 +0300

    Added timeout for processing requests to server

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from http.server import HTTPServer
 from pprint import pprint
 import json
 import sys
+import signal
 
 from apiServer import tionAPIserver
 
@@ -11,6 +12,24 @@ ADDRESS = "127.0.0.1"
 server_address = (ADDRESS, PORT)
 
 handler = tionAPIserver
+
+class limitedHttpServer(HTTPServer):
+  def _signal_handler(self, signum, frame):
+    raise Exception("Timed out!")
+
+  def serve_forever(self):
+    """Handle one request at a time until doomsday."""
+    while 1:
+      signal.signal(signal.SIGALRM, self._signal_handler)
+      signal.alarm(4)
+      try:
+        self.handle_request()
+      except Exception as e:
+        print("Got exceptin {}".format(str(e)))
+      finally:
+        signal.alarm(0)
+
+
 httpd = HTTPServer(server_address, tionAPIserver)
 httpd.serve_forever()
 
